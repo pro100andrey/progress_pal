59926898b16bb4678c371180e2a29a848445913e
Andrey Ivanov <this.andrey@gmail.com>
2025-08-23 01:07:19 +0300

    wip

--- ui/lib/pages/home_page.dart
+++ ui/lib/pages/home_page.dart
@@ -8,120 +8,127 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = ShadTheme.of(context);
-    final square = SizedBox.square(
-      dimension: 16,
-      child: Center(
-        child: SizedBox.square(
-          dimension: 8,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              color: theme.colorScheme.foreground,
-              shape: BoxShape.circle,
-            ),
-          ),
-        ),
-      ),
-    );
-    final divider = ShadSeparator.horizontal(
-      margin: const EdgeInsets.symmetric(vertical: 4),
-      color: theme.colorScheme.muted,
-    );
-
+    final today = DateTime.now();
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
-        title: ShadMenubar(
-          items: [
-            ShadMenubarItem(
-              items: [
-                const ShadContextMenuItem(child: Text('New Tab')),
-                const ShadContextMenuItem(child: Text('New Window')),
-                const ShadContextMenuItem(
-                  enabled: false,
-                  child: Text('New Incognito Window'),
-                ),
-                divider,
-                const ShadContextMenuItem(
-                  trailing: Icon(LucideIcons.chevronRight),
-                  items: [
-                    ShadContextMenuItem(child: Text('Email Link')),
-                    ShadContextMenuItem(child: Text('Messages')),
-                    ShadContextMenuItem(child: Text('Notes')),
-                  ],
-                  child: Text('Share'),
-                ),
-                divider,
-                const ShadContextMenuItem(child: Text('Print...')),
-              ],
-              child: const Text('File'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          spacing: 16,
+          children: [
+            const ShadSlider(
+              initialValue: 33,
+              max: 100,
             ),
-            ShadMenubarItem(
-              items: [
-                const ShadContextMenuItem(child: Text('Undo')),
-                const ShadContextMenuItem(child: Text('Redo')),
-                divider,
-                ShadContextMenuItem(
-                  trailing: const Icon(LucideIcons.chevronRight),
-                  items: [
-                    const ShadContextMenuItem(child: Text('Search the web')),
-                    divider,
-                    const ShadContextMenuItem(child: Text('Find...')),
-                    const ShadContextMenuItem(child: Text('Find Next')),
-                    const ShadContextMenuItem(child: Text('Find Previous')),
-                  ],
-                  child: const Text('Find'),
-                ),
-                divider,
-                const ShadContextMenuItem(child: Text('Cut')),
-                const ShadContextMenuItem(child: Text('Copy')),
-                const ShadContextMenuItem(child: Text('Paste')),
-              ],
-              child: const Text('Edit'),
+            const ShadSeparator.horizontal(
+              thickness: 4,
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              radius: BorderRadius.all(Radius.circular(4)),
             ),
-            ShadMenubarItem(
-              items: [
-                const ShadContextMenuItem.inset(
-                  child: Text('Always Show Bookmarks Bar'),
-                ),
-                const ShadContextMenuItem(
-                  leading: Icon(LucideIcons.check),
-                  child: Text('Always Show Full URLs'),
-                ),
-                divider,
-                const ShadContextMenuItem.inset(child: Text('Reload')),
-                const ShadContextMenuItem.inset(
-                  enabled: false,
-                  child: Text('Force Reload'),
-                ),
-                divider,
-                const ShadContextMenuItem.inset(
-                  child: Text('Toggle Full Screen'),
-                ),
-                divider,
-                const ShadContextMenuItem.inset(child: Text('Hide Sidebar')),
-              ],
-              child: const Text('View'),
+            ShadSwitchFormField(
+              id: 'terms',
+              initialValue: false,
+              inputLabel: const Text('I accept the terms and conditions'),
+              onChanged: (v) {},
+              inputSublabel: const Text(
+                'You agree to our Terms and Conditions',
+              ),
+              validator: (v) {
+                if (!v) {
+                  return 'You must accept the terms and conditions';
+                }
+                return null;
+              },
             ),
-            ShadMenubarItem(
-              items: [
-                const ShadContextMenuItem.inset(child: Text('Andy')),
-                ShadContextMenuItem(
-                  leading: square,
-                  child: const Text('Benoit'),
+            const ShadSeparator.horizontal(
+              thickness: 4,
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              radius: BorderRadius.all(Radius.circular(4)),
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 600),
+              child: const ShadTimePicker(
+                trailing: Padding(
+                  padding: EdgeInsets.only(left: 8, top: 14),
+                  child: Icon(LucideIcons.clock4),
                 ),
-                const ShadContextMenuItem.inset(child: Text('Luis')),
-                divider,
-                const ShadContextMenuItem.inset(child: Text('Edit...')),
-                divider,
-                const ShadContextMenuItem.inset(child: Text('Add Profile...')),
+              ),
+            ),
+
+            ShadCalendar(
+              selected: today,
+              fromMonth: DateTime(today.year - 1),
+              toMonth: DateTime(today.year, 12),
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 600),
+              child: const ShadDatePicker(),
+            ),
+            ShadTabs<String>(
+              value: 'account',
+              tabBarConstraints: const BoxConstraints(maxWidth: 400),
+              contentConstraints: const BoxConstraints(maxWidth: 400),
+              tabs: [
+                ShadTab(
+                  value: 'account',
+                  content: ShadCard(
+                    title: const Text('Account'),
+                    description: const Text(
+                      "Make changes to your account here. Click save when you're done.",
+                    ),
+                    footer: const ShadButton(child: Text('Save changes')),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const SizedBox(height: 16),
+                        ShadInputFormField(
+                          label: const Text('Name'),
+                          initialValue: 'Ale',
+                        ),
+                        const SizedBox(height: 8),
+                        ShadInputFormField(
+                          label: const Text('Username'),
+                          initialValue: 'nank1ro',
+                        ),
+                        const SizedBox(height: 16),
+                      ],
+                    ),
+                  ),
+                  child: const Text('Account'),
+                ),
+                ShadTab(
+                  value: 'password',
+                  content: ShadCard(
+                    title: const Text('Password'),
+                    description: const Text(
+                      "Change your password here. After saving, you'll be logged out.",
+                    ),
+                    footer: const ShadButton(child: Text('Save password')),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 16),
+                        ShadInputFormField(
+                          label: const Text('Current password'),
+                          obscureText: true,
+                        ),
+                        const SizedBox(height: 8),
+                        ShadInputFormField(
+                          label: const Text('New password'),
+                          obscureText: true,
+                        ),
+                        const SizedBox(height: 16),
+                      ],
+                    ),
+                  ),
+                  child: const Text('Password'),
+                ),
               ],
-              child: const Text('Profiles'),
             ),
           ],
         ),
       ),
-      body: const Center(child: Text('Home')),
     );
   }
 }
