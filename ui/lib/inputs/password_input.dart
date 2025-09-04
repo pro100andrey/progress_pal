d35ea1d5782e808425a2df708d27e0c756db3d85
Andrii Ivanov <thsis.andrey@gmail.com>
2025-09-04 13:10:56 +0300

    Cleanup after fix - https://github.com/nank1ro/flutter-shadcn-ui/issues/435

--- ui/lib/inputs/password_input.dart
+++ ui/lib/inputs/password_input.dart
@@ -2,26 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:localization/generated/l10n.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
-import 'input_form_field.dart';
+import '../models/value_changed.dart';
 
-class PasswordInput extends InputFormField {
-  PasswordInput({required super.vm, super.key})
-    : super(
-        id: 'password',
-        placeholder: Text(S.current.password),
-        leading: const Icon(LucideIcons.key100),
-        trailingBuilder: (state) => ShadButton(
-          width: 24,
-          height: 24,
-          padding: EdgeInsets.zero,
-          onPressed: state.toggleObscure,
-          child: Icon(
-            state.obscure ? LucideIcons.eyeOff200 : LucideIcons.eye200,
-          ),
-        ),
+class PasswordInput extends ShadInputFormField {
+  PasswordInput({
+    required this.vm,
+    String? id,
+    String? placeholder,
+    IconData? leadingIcon,
+    super.key,
+  }) : super(
+         id: id ?? 'password',
+         placeholder: Text(placeholder ?? S.current.password),
+         leading: Icon(leadingIcon ?? LucideIcons.key100),
+         trailing: Builder(
+           builder: (context) {
+             var obscure = true;
+             return StatefulBuilder(
+               builder: (context, setState) => ShadButton(
+                 width: 24,
+                 height: 24,
+                 padding: EdgeInsets.zero,
+                 onPressed: () {
+                   setState(() {
+                     obscure = !obscure;
+                   });
+                 },
+                 child: Icon(
+                   obscure ? LucideIcons.eyeOff200 : LucideIcons.eye200,
+                 ),
+               ),
+             );
+           },
+         ),
+         keyboardType: TextInputType.visiblePassword,
+         obscureText: true,
+         autofillHints: const [AutofillHints.password],
+         initialValue: vm.value,
+         onChanged: vm.onChanged,
+       );
 
-        keyboardType: TextInputType.visiblePassword,
-        obscureText: true,
-        autofillHints: const [AutofillHints.password],
-      );
+  final ValueChangedVm<String?> vm;
 }
